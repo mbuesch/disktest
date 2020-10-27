8fe463fc1263d464369beabaeb2b5fbf93be25d2
Michael Buesch <m@bues.ch>
2020-10-27 21:00:43 +0100

    Add more documentation

---
+++ maintenance/gen-doc.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+#
+# Generate documentation
+#
+
+
+basedir="$(dirname "$0")"
+[ "$(echo "$basedir" | cut -c1)" = '/' ] || basedir="$PWD/$basedir"
+
+srcdir="$basedir/.."
+
+
+die()
+{
+	echo "$*" >&2
+	exit 1
+}
+
+gen()
+{
+	local rst="$1"
+	local docname="$(basename "$rst" .rst)"
+	local dir="$(dirname "$rst")"
+	local html="$dir/$docname.html"
+
+	echo "Generating $(realpath --relative-to="$srcdir" "$html") from $(realpath --relative-to="$srcdir" "$rst") ..."
+
+	python3 -m readme_renderer -o "$html" "$rst" ||\
+		die "Failed to generate"
+}
+
+for i in $(find "$srcdir" \( -name release-archives -prune \) -o \( -name target -prune \) -o \( -name '*.rst' -print \)); do
+	gen "$i"
+done
+
+cd "$srcdir" || die "cd failed."
+cargo doc || die "cargo doc failed."
+
+exit 0
