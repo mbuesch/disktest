ec521cbf8ba3f28581be530e52f692b7a472d52b
Michael Buesch <m@bues.ch>
2020-11-10 21:20:08 +0100

    makerelease: default_archives is not required.

--- maintenance/makerelease.sh
+++ maintenance/makerelease.sh
@@ -5,10 +5,9 @@ srcdir="$(dirname "$0")"
 
 srcdir="$srcdir/.."
 
-die() { echo "$*"; exit 1; }
-
 # Import the makerelease.lib
 # https://bues.ch/cgit/misc.git/tree/makerelease.lib
+die() { echo "$*"; exit 1; }
 for path in $(echo "$PATH" | tr ':' ' '); do
 	[ -f "$MAKERELEASE_LIB" ] && break
 	MAKERELEASE_LIB="$path/makerelease.lib"
@@ -16,5 +15,4 @@ done
 [ -f "$MAKERELEASE_LIB" ] && . "$MAKERELEASE_LIB" || die "makerelease.lib not found."
 
 project=disktest
-default_archives=tar.xz
 makerelease "$@"
