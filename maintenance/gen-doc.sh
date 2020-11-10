cd17aa4d7b7c9c2b5cc73361f43e58893171bdfa
Michael Buesch <m@bues.ch>
2020-11-10 21:04:01 +0100

    Generate markdown documentation from rst

--- maintenance/gen-doc.sh
+++ maintenance/gen-doc.sh
@@ -22,11 +22,15 @@ gen()
 	local docname="$(basename "$rst" .rst)"
 	local dir="$(dirname "$rst")"
 	local html="$dir/$docname.html"
+	local md="$dir/$docname.md"
 
 	echo "Generating $(realpath --relative-to="$srcdir" "$html") from $(realpath --relative-to="$srcdir" "$rst") ..."
-
 	python3 -m readme_renderer -o "$html" "$rst" ||\
-		die "Failed to generate"
+		die "Failed to generate HTML."
+
+	echo "Generating $(realpath --relative-to="$srcdir" "$md") from $(realpath --relative-to="$srcdir" "$rst") ..."
+	pandoc -s -o "$md" "$rst" ||\
+		die "Failed to generate MD."
 }
 
 for i in $(find "$srcdir" \( -name release-archives -prune \) -o \( -name target -prune \) -o \( -name '*.rst' -print \)); do
