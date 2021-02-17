e3eeb24decf60d9c9708ee89df81b19c5ddba3ed
Michael Buesch <m@bues.ch>
2021-02-17 20:49:44 +0100

    args: Small cleanup

--- src/args.rs
+++ src/args.rs
@@ -80,7 +80,7 @@ const HELP_THREADS: &str = "\
 The number of CPUs to use. \
 The special value 0 will select the maximum number of online CPUs in the system. \
 If the number of threads is equal to number of CPUs it is optimal for performance. \
-This parameter must be equal during corresponding verify and --write mode runs. \
+This parameter must be equal during corresponding verify and write mode runs. \
 Otherwise the verification will fail. Default: 1";
 
 const HELP_QUIET: &str = "\
