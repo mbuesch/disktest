781893aacebc75ec1d0b8724b863092c010a645f
Michael Buesch <m@bues.ch>
2024-03-18 20:23:53 +0100

    Split release script

--- maintenance/makerelease-disktest-rawio.sh
+++ maintenance/makerelease-disktest-rawio.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-srcdir="$(realpath -e "$0" | xargs dirname)"
+srcdir="$(realpath "$0" | xargs dirname)"
 srcdir="$srcdir/.."
 
 # Import the makerelease.lib
@@ -10,21 +10,8 @@ for path in $(echo "$PATH" | tr ':' ' '); do
 	[ -f "$MAKERELEASE_LIB" ] && break
 	MAKERELEASE_LIB="$path/makerelease.lib"
 done
-
 [ -f "$MAKERELEASE_LIB" ] && . "$MAKERELEASE_LIB" || die "makerelease.lib not found."
+
 project=disktest-rawio
 conf_package=disktest-rawio
-conf_notag=1
-makerelease "$@"
-
-[ -f "$MAKERELEASE_LIB" ] && . "$MAKERELEASE_LIB" || die "makerelease.lib not found."
-project=disktest-lib
-conf_package=disktest-lib
-conf_notag=1
-makerelease "$@"
-
-[ -f "$MAKERELEASE_LIB" ] && . "$MAKERELEASE_LIB" || die "makerelease.lib not found."
-project=disktest
-conf_package=disktest
-conf_notag=0
 makerelease "$@"
