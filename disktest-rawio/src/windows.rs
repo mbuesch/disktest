93d4507a4b31f57ff48dd7c72ce4c0847226f08e
Michael Büsch <m@bues.ch>
2026-03-08 09:56:24 +0100

    Update file headers

--- disktest-rawio/src/windows.rs
+++ disktest-rawio/src/windows.rs
@@ -2,7 +2,7 @@
 //
 // disktest - Storage tester
 //
-// Copyright 2020-2024 Michael Büsch <m@bues.ch>
+// Copyright 2020-2026 Michael Büsch <m@bues.ch>
 //
 // Licensed under the Apache License version 2.0
 // or the MIT license, at your option.
